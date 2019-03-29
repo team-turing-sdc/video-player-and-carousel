c49def31842f0033d28ff193a9d99f28f3cb5804
Brock Ross <m.brock.ross@gmail.com>
2019-03-28 18:10:55 -0600

    Adds url parsing logic to VideoPlayer to turn '/watch?v=...' url into an embed-formatted url

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -3,11 +3,18 @@ import React from 'react';
 class VideoPlayer extends React.Component {
   constructor(props) {
     super(props);
+
+    this.video = this.props.video;
   }
 
+
   render() {
+    //"https://www.youtube.com/watch?v=UMaAF2WTK1A" --> "https://www.youtube.com/embed/UMaAF2WTK1A"
+    let youtubeURL = "https://www.youtube.com/embed/";
+    let vidID = this.video.url.split('=')[1];
+
     return (
-      <iframe width="420" height="315" src={this.props.video.url}>
+      <iframe width="420" height="315" src={`${youtubeURL}${vidID}`}>
       </iframe>
     )
   }
