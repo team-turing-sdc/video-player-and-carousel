92b2bd66a24111ef05bcc1e42b10ec8e806f2ca4
Brock Ross <m.brock.ross@gmail.com>
2019-03-30 07:51:03 -0600

    Adds border to carousel box

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -4,11 +4,17 @@ import styled from 'styled-components';
 import CarouselItem from './CarouselItem.jsx';
 
 //======= STYLES =======//
+const CarouselBox = styled.div`
+  border: 2px solid slategrey;
+  width: 90%;
+`
+
 const ItemContainer = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: space-around;
   width: 80%;
+  padding: 5px 0px;
 `
 
 //======= COMPONENT =======//
@@ -21,11 +27,14 @@ class Carousel extends React.Component {
 
   render() {
     return(
-      <ItemContainer>
-       {this.props.videos.map((vid, idx) => {
-         return (<CarouselItem key={idx} video={vid}/>)
-       })}
-      </ItemContainer>
+      <CarouselBox>
+        <ItemContainer>
+        {this.props.videos.map((vid, idx) => {
+          return (<CarouselItem key={idx} video={vid}/>)
+        })}
+        </ItemContainer>
+      </CarouselBox>
+
     )
   }
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App.jsx';
 
-const MOVIE_ID = 20; //hardcode for testing; should be modifiable by some external means later on
+const MOVIE_ID = 8; //hardcode for testing; should be modifiable by some external means later on
 
 ReactDOM.render(<App movieID={MOVIE_ID}/>, document.getElementById('app'));
