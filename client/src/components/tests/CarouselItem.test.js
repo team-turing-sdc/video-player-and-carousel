45970d23c0dadb410892abec7489a65e7be06b01
Brock Ross <m.brock.ross@gmail.com>
2019-04-01 18:11:19 -0600

    Moves tests to their own folder to better comply with deliverable prompt ('link to test folder on github')

--- client/src/components/tests/CarouselItem.test.js
+++ client/src/components/tests/CarouselItem.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {mount} from 'enzyme';
 
-import CarouselItem from './CarouselItem';
+import CarouselItem from '../CarouselItem';
 
 xdescribe('Carousel Item', () => {
   it('should mount correctly', () => {
-    const component = mount(<CarouselItem/>)
+    const component = mount(<CarouselItem />)
 
     expect(component).toMatchSnapshot();
   })
