d1b2247c016dcd08bf54c25bffa644477cc1d3a8
Brock Ross <m.brock.ross@gmail.com>
2019-04-03 19:09:59 -0600

    Completes addition of thumbnail click feature (clicking a video thumbnail in the carousel loads that video into the player)

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App.jsx';
 
-const MOVIE_ID = 37; //hardcode for testing; should be modifiable by some external means later on
+const MOVIE_ID = 12; //hardcode for testing; should be modifiable by some external means later on
 
 ReactDOM.render(<App movieID={MOVIE_ID}/>, document.getElementById('app'));
