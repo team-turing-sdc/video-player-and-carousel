869bf2887d9129f334bb27eb90aa32bc9dcf1bed
Brock Ross <m.brock.ross@gmail.com>
2019-04-11 17:19:37 -0600

    Code cleanup on getMovieData function

--- client/src/getMovieData.js
+++ client/src/getMovieData.js
@@ -1,12 +1,13 @@
 const getMovieData = async (id  = 1) => {
   try {
     const response = await fetch(`http://localhost:3333/associatedVideos?movieID=${id}`, {
-    method: "GET",
-    headers: {
-      "Content-Type":"application/json",
-      "Cache-Control":"no-cache"
-    }
-  })
+      method: "GET",
+      headers: {
+        "Content-Type":"application/json",
+        "Cache-Control":"no-cache"
+      }
+    })
+
     const movieData = await response.json();
     console.log(`movie data: `, movieData[0]);
     return movieData[0];
