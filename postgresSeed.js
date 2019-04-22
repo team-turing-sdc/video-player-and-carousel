e70607f57ceb0daef284cdced0b222497f69ffb4
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-04-22 19:22:07 -0400

    create schema for movieData table

---
+++ postgresSeed.js
@@ -0,0 +1,16 @@
+
+
+CREATE TABLE movieData(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR,
+  associatedVideoTitle1 VARCHAR,
+  associatedVideoLink1 VARCHAR,
+  associatedVideoTitle2 VARCHAR,
+  associatedVideoLink2 VARCHAR,
+  associatedVideoTitle3 VARCHAR,
+  associatedVideoLink3 VARCHAR,
+  associatedVideoTitle4 VARCHAR,
+  associatedVideoLink4 VARCHAR,
+  associatedVideoTitle5 VARCHAR,
+  associatedVideoLink5 VARCHAR
+  )
