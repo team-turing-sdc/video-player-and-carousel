b37a396204a155f523dd6427f8581fa8bdbd636a
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-04-25 23:04:03 -0400

    start transitioning from mongoose to sequelize

--- server/db/index.js
+++ server/db/index.js
@@ -15,24 +15,71 @@ sequelize
     console.error('Unable to connect to the database:', err);
 });
 
+class Movie extends Model {};
+Movie.init({
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING
+  },
+  associatedVideoTitle1: {
+    type: Sequelize.STRING
+  },
+  associatedVideoLink1: {
+    type: Sequelize.STRING
+  },
+  associatedVideoTitle2: {
+    type: Sequelize.STRING
+  },
+  associatedVideoLink2: {
+    type: Sequelize.STRING
+  },
+  associatedVideoTitle3: {
+    type: Sequelize.STRING
+  },
+  associatedVideoLink3: {
+    type: Sequelize.STRING
+  },
+  associatedVideoTitle4: {
+    type: Sequelize.STRING
+  },
+  associatedVideoLink4: {
+    type: Sequelize.STRING
+  },
+  associatedVideoTitle5: {
+    type: Sequelize.STRING
+  },
+  associatedVideoLink5: {
+    type: Sequelize.STRING
+  }
+}, {
+  sequelize,
+  modelName: 'user'
+});
+
+
+// let getMovieData = (id, callback) => {
+//   let query = Movie.find({id: id});
+
+//   query.exec((err, docs) => {
+//     if (err) {
+//       console.log('error querying mongodb: ', err);
+//       callback(err, null);
+//     } else {
+//       console.log('mongodb query successful!');
+//       callback(null, docs);
+//     }
+//   })
+// }
 
 let getMovieData = (id, callback) => {
-  let query = Movie.find({id: id});
-
-  query.exec((err, docs) => {
-    if (err) {
-      console.log('error querying mongodb: ', err);
-      callback(err, null);
-    } else {
-      console.log('mongodb query successful!');
-      callback(null, docs);
-    }
-  })
-}
-
-let getMovieData = (id, callback) => {
-
-}
+  let query = `SELECT * FROM moviedata WHERE id=${id}`;
+  sequelize.query(query).then(movie => {
+    callback(null, movie);
+  }).catch(err => err ? callback(err) : callback(null));
+};
 
 module.exports.getMovieData = getMovieData;
 
