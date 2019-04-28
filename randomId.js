44da0c3436badc4c34ff2f88e9f9357c588d160f
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-04-27 21:53:15 -0400

    add generateRandomGetId to the module exports

--- randomId.js
+++ randomId.js
@@ -1,5 +1,8 @@
-
 let generateRandomGetId = (userContext, events, done) {
   userContext.vars.getId = Math.random() * (10000000 - 9999000) + 9999000;
   return done();
+}
+
+module.exports = {
+  generateRandomGetId
 }
