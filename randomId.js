cb9b7fa52ee2f80506409751b1effa4b86b1d1d1
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-04-28 17:20:59 -0400

    create generate post id function

--- randomId.js
+++ randomId.js
@@ -1,8 +1,17 @@
+let postId = 10000001;
+
 function generateRandomGetId(userContext, events, done) {
   let getId = Math.random() * (10000000 - 9999000) + 9999000;
   userContext.vars.getId = getId;
   return done();
 }
+
+function generateRandomPostId(userContext, events, done) {
+  postId++;
+  userContext.vars.postId = postId;
+  return done();
+}
+
 
 module.exports = {
   generateRandomGetId
