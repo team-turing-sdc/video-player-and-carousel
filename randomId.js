52cc704a09e5b4ca67782cabe66720da545a189f
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-04-27 21:55:57 -0400

    add variable that holds id value

--- randomId.js
+++ randomId.js
@@ -1,5 +1,6 @@
 let generateRandomGetId = (userContext, events, done) {
-  userContext.vars.getId = Math.random() * (10000000 - 9999000) + 9999000;
+  let getId = Math.random() * (10000000 - 9999000) + 9999000;
+  userContext.vars.getId = getId;
   return done();
 }
 
