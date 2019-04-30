830d07366ed5db0d07da80f2f7c7aaa0051b7876
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-04-30 19:23:27 -0400

    comment out synopsis rendering

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,4 +7,4 @@ import Synopsis from './components/Synopsis.jsx'
 const MOVIE_ID = 1; //hardcode for testing; should be modifiable by some external means later on
 
 ReactDOM.render(<App movieID={MOVIE_ID}/>, document.getElementById('video'));
-ReactDOM.render(<Synopsis movieID={MOVIE_ID}/>, document.getElementById('synopsis'));
+// ReactDOM.render(<Synopsis movieID={MOVIE_ID}/>, document.getElementById('synopsis'));
